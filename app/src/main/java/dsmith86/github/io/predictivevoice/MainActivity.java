d60b0665806cbc4296a470adfeb767db38af9ad0
Daniel Smith <danielsmith4483@gmail.com>
2015-01-21 20:55:08 -0600

    Implement basic speech recognition

--- app/src/main/java/dsmith86/github/io/predictivevoice/MainActivity.java
+++ app/src/main/java/dsmith86/github/io/predictivevoice/MainActivity.java
@@ -1,7 +1,12 @@
 package dsmith86.github.io.predictivevoice;
 
+import android.content.Intent;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,12 +14,16 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends ActionBarActivity {
 
     private Button mainButton;
     private TextView currentSpeechTextView;
     private ListView suggestionListView;
+    private SpeechRecognizer speechRecognizer;
+    Intent speechRecognitionIntent;
 
     Boolean listening;
 
@@ -57,18 +66,89 @@ public class MainActivity extends ActionBarActivity {
         suggestionListView = (ListView)findViewById(R.id.suggestionListView);
 
         mainButton.setOnClickListener(mainButtonOnClickListener);
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        speechRecognizer.setRecognitionListener(recognitionListener);
+
+        speechRecognitionIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognitionIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognitionIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, "voice.recognition.test");
+        speechRecognitionIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
+        speechRecognitionIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
+    }
+
+    private void toggleSpeechState() {
+        listening = !listening;
+
+        if (listening) {
+            mainButton.setText(getResources().getString(R.string.button_stopListening));
+            speechRecognizer.startListening(speechRecognitionIntent);
+        } else {
+            mainButton.setText(getResources().getString(R.string.button_listen));
+            speechRecognizer.stopListening();
+        }
     }
 
     private Button.OnClickListener mainButtonOnClickListener = new Button.OnClickListener() {
         @Override
         public void onClick(View v) {
-            listening = !listening;
+            toggleSpeechState();
+        }
+    };
 
-            if (listening) {
-                mainButton.setText(getResources().getString(R.string.button_stopListening));
-            } else {
-                mainButton.setText(getResources().getString(R.string.button_listen));
+    private RecognitionListener recognitionListener = new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            Log.d("debug", "ready for speech");
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+            Log.d("debug", "beginning speech");
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB) {
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+            Log.d("debug", "buffer received");
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            toggleSpeechState();
+        }
+
+        @Override
+        public void onError(int error) {
+            switch (error) {
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                    if (listening) {
+                        currentSpeechTextView.setText(getResources().getString(R.string.status_nudge));
+                    }
+                    break;
+                default:
+                    break;
             }
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            ArrayList<String> resultsArray = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            currentSpeechTextView.setText(resultsArray.get(0));
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+            ArrayList<String> resultsArray = partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            currentSpeechTextView.setText(resultsArray.get(0));
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+            Log.d("debug", "speech event");
         }
     };
 }
